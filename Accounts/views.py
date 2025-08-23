c30658015004f613621f508a2ed79e3b104a48d3
Hasan Mehedi <drzraju@gmail.com>
2025-08-24 03:04:06 +0600

    Add SurveyListCreateView

--- Accounts/views.py
+++ Accounts/views.py
@@ -26,3 +26,8 @@ class LoginView(APIView):
         if serializer.is_valid():
             return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SurveyListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = OnboardingSurvey.objects.all()
+    serializer_class = SurveySerializer
