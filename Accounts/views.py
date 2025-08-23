046237e16f1fac650d79ebeba17399b4e021af13
Hasan Mehedi <drzraju@gmail.com>
2025-08-24 03:03:17 +0600

    Add LoginView

--- Accounts/views.py
+++ Accounts/views.py
@@ -17,3 +17,12 @@ class UserUpdateView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_object(self):
         return self.request.user
+    
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
