c315bf274ce018143c7db9fa85d0298a4a6942b9
Hasan Mehedi <drzraju@gmail.com>
2025-08-24 03:00:03 +0600

    Add UserCreateView

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
 # Create your views here.
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
