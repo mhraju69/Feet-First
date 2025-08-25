b006dac8fbb953582ab1b597b20649cd7497ac0f
Hasan Mehedi <drzraju@gmail.com>
2025-08-26 04:22:06 +0600

    add SubcategoryListView

--- Products/views.py
+++ Products/views.py
@@ -7,9 +7,18 @@ from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 
-class CategoryCreateView(generics.ListAPIView):
+class CategoryListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = CategorySerializer
     filter_backends = [OrderingFilter]
     ordering = ['name']
     queryset = Category.objects.all()
+
+
+class SubcategoryListView(generics.ListAPIView):
+    serializer_class = SubCategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = {'category'}
+    ordering = ['name'] 
+    queryset = SubCategory.objects.all()
