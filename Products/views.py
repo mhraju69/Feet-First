5617073d9d18ad51c0544f963c8271e9157cfe74
Hasan Mehedi <drzraju@gmail.com>
2025-08-26 04:21:00 +0600

    add CategoryCreateView

--- Products/views.py
+++ Products/views.py
@@ -1,3 +1,15 @@
+from .models import *
+from .serializers import *
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
+
+class CategoryCreateView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CategorySerializer
+    filter_backends = [OrderingFilter]
+    ordering = ['name']
+    queryset = Category.objects.all()
