dff3f130a34aacac6de60fdb4a1e3cac3d1f8e1c
Hasan Mehedi <drzraju@gmail.com>
2025-08-26 04:16:17 +0600

    add Category models

--- Products/models.py
+++ Products/models.py
@@ -7,6 +7,5 @@ class Category(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='category_images/',default='category_images/default.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
