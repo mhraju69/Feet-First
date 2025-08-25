bf60d70f3cf43659341a08f04c4889af5f336865
Hasan Mehedi <drzraju@gmail.com>
2025-08-26 04:17:38 +0600

    add Subcategory models

--- Products/models.py
+++ Products/models.py
@@ -9,3 +9,14 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
+    
+class SubCategory(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='category_images/',default='category_images/default.jpg')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='subcategory')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
