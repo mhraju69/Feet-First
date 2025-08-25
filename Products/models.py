29a1f8a56b9a1f352e468483188d40c98ee0d75c
Hasan Mehedi <drzraju@gmail.com>
2025-08-26 04:13:45 +0600

    add from django.db import models

--- Products/models.py
+++ Products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='category_images/',default='category_images/default.jpg')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
