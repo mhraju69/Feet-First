2bedb4d0535bc4ab99b1dbf41c90edee72e15f17
Hasan Mehedi <drzraju@gmail.com>
2025-08-28 03:46:32 +0600

    update product model

--- Products/models.py
+++ Products/models.py
@@ -20,15 +20,19 @@ class SubCategory(models.Model):
         return self.name_de 
     
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name_de = models.CharField(max_length=200)
+    name_it= models.CharField(max_length=200)
+
+    german_description = models.TextField(blank=True, null=True)
+    intalian_description = models.TextField(blank=True, null=True)
+
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(blank=True, null=True)
     discount = models.DecimalField(max_digits=5, decimal_places=2, default=0)  # percentage discount
     stock_quantity = models.PositiveIntegerField(default=0)
     is_active = models.BooleanField(default=True)
     brand = models.CharField(max_length=255, blank=True, null=True)
-    available_sizes = models.JSONField(default=list, blank=True)  # e.g. ["S", "M", "L"]
-    available_widths = models.JSONField(default=list, blank=True)  # e.g. ["Narrow", "Regular", "Wide"]
+    sizes = models.ManyToManyField(AvailableSize)
+    widths = models.ManyToManyField(AvailableWidth) 
     toe_type = models.CharField(max_length=100, blank=True, null=True)
     heel_angle = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     arch_support = models.BooleanField(default=False)
@@ -38,4 +42,4 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.name_de 
