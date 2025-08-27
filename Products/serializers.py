99733eb0be9e7e283690f7d0e295ff0ce613900b
Hasan Mehedi <drzraju@gmail.com>
2025-08-28 03:50:12 +0600

    update ProductSerializer  model

--- Products/serializers.py
+++ Products/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import *
 
 class ProductSerializer(serializers.ModelSerializer):
+    sizes = serializers.StringRelatedField(many=True)
+    widths = serializers.StringRelatedField(many=True)
     class Meta:
         model = Product
         fields = '__all__'
