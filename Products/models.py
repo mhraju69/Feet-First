7d3977dd3ba59de7223ccf54421e879e43fd9b59
Hasan Mehedi <drzraju@gmail.com>
2025-08-28 03:47:19 +0600

    add AvailableSize model

--- Products/models.py
+++ Products/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
+class AvailableSize(models.Model):
+    size = models.CharField(max_length=10, unique=True)
+    details = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.size 
+    
 class Category(models.Model):
     name_de = models.CharField(max_length=200)
     name_it= models.CharField(max_length=200)
