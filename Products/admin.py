62886aefd13688a7bfda623bb20bd0cf2d569699
Hasan Mehedi <drzraju@gmail.com>
2025-08-28 05:16:20 +0600

    update admin.py

--- Products/admin.py
+++ Products/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import *
-
+from unfold.admin import ModelAdmin
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(AvailableSize)
-admin.site.register(AvailableWidth)
-admin.site.register(PdfFile)
-admin.site.register(Category)
-admin.site.register(SubCategory)
+admin.site.register(Product,ModelAdmin)
+admin.site.register(AvailableSize,ModelAdmin)
+admin.site.register(AvailableWidth,ModelAdmin)
+admin.site.register(PdfFile,ModelAdmin)
+admin.site.register(Category,ModelAdmin)
+admin.site.register(SubCategory,ModelAdmin)
