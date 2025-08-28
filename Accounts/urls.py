f0a1e1bbe1554094ea2160df114c53ed73f27521
Hasan Mehedi <drzraju@gmail.com>
2025-08-28 23:57:56 +0600

    Update Urls.py for logout path

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,12 +2,13 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('verify-otp/',VerifyOTP.as_view()),
-    path('get-otp/',Get_otp.as_view()),
+    path('get-otp/',Get_otp.as_view(), name="get_otp"),
+    path('verify-otp/',VerifyOTP.as_view(), name="verify_otp"),
+    path('signup/',UserCreateView.as_view(), name="signup"),
     path('reset-password/',Reset_password.as_view()),
-    path('signup/',UserCreateView.as_view()),
-    path("login/", LoginView.as_view(), name="login"),
     path('user-update/',UserUpdateView.as_view()),
+    path("login/", LoginView.as_view(), name="login"),
+    path("api/logout/", LogoutView.as_view(), name="logout"),
     path("surveys/", SurveyListCreateView.as_view(), name="survey-list-create"),
     path("addresses/", AddressListCreateView.as_view(), name="address-list-create"),
     path("addresses/<int:pk>/", AddressDetailView.as_view(), name="address-detail"),
--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
