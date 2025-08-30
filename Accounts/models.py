ca7cf152d82b51547f7c08c40370261cc97b3aa5
Hasan Mehedi <drzraju@gmail.com>
2025-08-31 01:14:20 +0600

    add OnboardingSurvey models

--- Accounts/models.py
+++ Accounts/models.py
@@ -106,29 +106,6 @@ class OTP(models.Model):
         self.last_tried = timezone.now()
         self.save() 
   
-class OnboardingSurvey(models.Model):
-
-    # Foreign key to the user
-    user =  models.OneToOneField(User, on_delete=models.CASCADE, related_name="survey")
-
-    sources = models.JSONField(default=list, blank=True)  # 👈 multiple choices allowed
-
-    # Step 3: Which products do you use the most?
-    PRODUCT_CHOICES = [
-        ("man", "Man"),
-        ("woman", "Woman"),
-    ]
-    product_preference = models.CharField(max_length=10, choices=PRODUCT_CHOICES)
-
-    # Step 4: Free text for foot/shoe problems
-    foot_problems = models.TextField(blank=True, null=True)
-
-    # Metadata
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Survey of {self.user.email}"
-
 class Address(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, 
--- Surveys/models.py
+++ Surveys/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class OnboardingSurvey(models.Model):
+
+    # Foreign key to the user
+    user =  models.OneToOneField(User, on_delete=models.CASCADE, related_name="survey")
+
+    sources = models.JSONField(default=list, blank=True)  # 👈 multiple choices allowed
+
+    # Step 3: Which products do you use the most?
+    PRODUCT_CHOICES = [
+        ("man", "Man"),
+        ("woman", "Woman"),
+    ]
+    product_preference = models.CharField(max_length=10, choices=PRODUCT_CHOICES)
+
+    # Step 4: Free text for foot/shoe problems
+    foot_problems = models.TextField(blank=True, null=True)
+
+    # Metadata
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Survey of {self.user.email}"
