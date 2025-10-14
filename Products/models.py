bda828cc842ad4c0d74677c7bb995ea56dce7604
Hasan Mehedi <drzraju@gmail.com>
2025-10-14 23:43:19 +0600

    update final code

--- Products/models.py
+++ Products/models.py
@@ -87,8 +87,8 @@ class Product(models.Model):
     description = models.TextField()
 
     # Category/Subcategory
-    main_category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE , null=True, blank=True)
-    sub_category = models.ForeignKey(SubCategory, related_name='sub_category', on_delete=models.CASCADE , null=True, blank=True)
+    main_category = models.ForeignKey(Category, related_name='category', on_delete=models.CASCADE )
+    sub_category = models.ForeignKey(SubCategory, related_name='sub_category', on_delete=models.CASCADE )
 
     # sizes = models.ManyToManyField(SizeTable, related_name="products")
     gender = models.TextField(max_length=20, choices=(('male','Male'),('female','Female')))
